67802982d23e808281ee1e7496cce334c46ee143
TAK2004 <thomas@ptscientists.com>
2014-07-13 23:03:34 +0200

    -added AddDependency script to mark dependencies to other Radon cmake framework targets -added GenerateCustomTargetMetaInfo to integrate external CMake based projects -the git revision script works now as intended

--- util/git.cmake
+++ util/git.cmake
@@ -2,16 +2,14 @@
 # This module will looking for svn command line executable and
 # obtain the svn revision and build version number of a specified repo.
 #
-FIND_PROGRAM(SVN_EXECUTABLE svn DOC "subversion command line client")
-mark_as_advanced(SVN_EXECUTABLE)
+FIND_PROGRAM(GIT_EXECUTABLE git DOC "git command line client")
+mark_as_advanced(GIT_EXECUTABLE)
 
-MACRO(Subversion_GET_REVISION dir variable)
-  EXECUTE_PROCESS(COMMAND ${SVN_EXECUTABLE} info ${dir}
+MACRO(GIT_GET_REVISION dir variable)
+  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} -C ${dir} rev-list HEAD --count
 	OUTPUT_VARIABLE ${variable}
 	OUTPUT_STRIP_TRAILING_WHITESPACE)
-  STRING(REGEX REPLACE "^(.*\n)?Revision: ([^\n]+).*"
-	"\\2" ${variable} "${${variable}}")
 	IF("${variable}" STREQUAL "")
 		SET(${variable} "0")
 	ENDIF("${variable}" STREQUAL "")
-ENDMACRO(Subversion_GET_REVISION)
+ENDMACRO(GIT_GET_REVISION)
